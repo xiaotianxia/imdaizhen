24d1b98ec8a29cc784be10357b4fc9ef41c0c0cc
xiaotianxia <434523316@qq.com>
2015-05-12 15:53:43 +0800

    new

---
+++ resume/js/dataOperation.js
@@ -0,0 +1,32 @@
+
+//加载项目信息  中文版
+function projectsLoad(jsonName,tableClass){
+	var jsonLen=jsonName.length;
+	var trString="<tr>";
+	for(var i=0;i<jsonLen;i++){
+		trString+="<td align='center' id='"+i+"' onclick='showProjectDetail(this);'><img src='images/"
+				+(jsonName[i].picName==''?"noPic":jsonName[i].picName)
+				+".jpg' style='width:200px;height:150px;' alt='"+jsonName[i].info
+				+" title='点击查看详情'/>"
+				+"<p style='color:#000;'>"+jsonName[i].info+"</p></td>"
+		if(i%3==2){
+			trString+="</tr>";
+		}
+	}
+	$(trString).appendTo("."+tableClass+" tbody");
+}
+
+
+              
+//显示项目详情
+function showProjectDetail(e){
+	popWin("projectDetailDiv");
+	var $detailDiv=$("#projectDetailDiv");
+	var $index=$(e).attr("id");
+	var $projectDetail=projectsInfo[$index].detail
+					  +"\nwebsite:<a href='"+projectsInfo[$index].url
+					  +"' target='_blank'>"+projectsInfo[$index].url
+					  +"</a>";
+	$detailDiv.html($projectDetail);
+}
+
---
+++ resume/js/popWin.js
@@ -0,0 +1,70 @@
+//------id:要弹出的div的id-----------
+//------调用之前将div的display改为none-----------
+//-------2015.01.28  by dz-----------------
+function popWin(id){	
+	//出现遮罩
+	var $mask=$('<div id="mask"></div>');
+	var wDoc=$(document).width();
+	var hDoc=$(document).height();
+	$mask.css({
+		"position":"absolute",
+		"width":wDoc,
+		"height":hDoc,
+		"top":0,
+		"left":0,
+		"background": "#000",
+		"filter":"alpha(opacity=40)",
+		"-moz-opacity":0.3,
+		"opacity":0.3,
+		"zIndex": 998
+	});
+	$mask.appendTo("body");
+	//出现弹窗
+	var $Win=$(document.getElementById(id));
+	var wWin=$Win.width();
+	var hWin=$Win.height();
+	var lWin=parseInt((wDoc-wWin)/2);
+	var tWin=parseInt((hDoc-hWin)/2);
+	var $winCont=$('<div id="winCont"></div>');
+	$winCont.css({
+		"position":"absolute",
+		"left":lWin,
+		"top":tWin-200,
+		"display":"block",
+		"padding":"5px",
+		"max-height":"600",
+		"background":"#ddd",
+		"border":"10px solid #eee",
+		"border-radius":"5px",
+		"zIndex":999
+	});
+	$Win.css({
+		"marginTop":16,
+		"display":"block",
+		"marginTop":-2,
+		"padding":25,
+		"overflow":"auto",
+		"border":"1px dashed #009ad6",
+		"zIndex":999
+	});
+	$Win.appendTo($winCont);
+	$winCont.appendTo("body");
+	//close
+	$mask.click(function(){
+		$winCont.hide();
+		$mask.remove();
+	})
+	//窗口变化
+	$(window).resize(function(){
+			if($mask&&$Win){
+				var wDoc=$(document).width();
+				var hDoc=$(document).height();
+				$mask.css({"width":wDoc,"height":hDoc});
+				var wWin=$Win.width();
+				var hWin=$Win.height();
+				var lWin=parseInt((wDoc-wWin)/2);
+				var tWin=parseInt((hDoc-hWin)/2);
+				$Win.css({"left":lWin,"top":tWin});	
+			}
+		});
+}
